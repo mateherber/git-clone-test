a1f0a519ea4d162fc7587e03dcadc953b6535671
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-07-15 15:08:13 +0200

    sample app generated

---
+++ git-clone-sample/git-clone-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-clone-sample
+//
+//  Created by Krisztian Goedrei on 15/07/16.
+//  Copyright © 2016 Krisztian Goedrei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
